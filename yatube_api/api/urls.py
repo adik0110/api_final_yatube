41694a256f9122803b4b4744b75813614c9d1751
adelsalamov <adelsalyamov@gmail.com>
2025-04-15 20:03:03 +0300

    done

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
+
+router = routers.DefaultRouter()
+router.register('posts', PostViewSet)
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment')
+router.register('groups', GroupViewSet)
+router.register('follow', FollowViewSet)
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,41 @@
-# TODO:  Напишите свой вариант
+from rest_framework import viewsets, permissions
+from posts.models import Post, Comment, Group, Follow
+from .serializers import PostSerializer, CommentSerializer, GroupSerializer, FollowSerializer
+from .permissions import IsAuthorOrReadOnly
+from django.shortcuts import get_object_or_404
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (IsAuthorOrReadOnly,)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get('post_id')
+        post = get_object_or_404(Post, id=post_id)
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post_id = self.kwargs.get('post_id')
+        serializer.save(author=self.request.user, post_id=post_id)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class FollowViewSet(viewsets.ModelViewSet):
+    queryset = Follow.objects.all()
+    serializer_class = FollowSerializer
+    http_method_names = ['get', 'post']
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
